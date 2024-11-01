89c6047d30f8a79d1158387392b60d1df6e02f9b
blank648 <54593183+blank648@users.noreply.github.com>
2024-11-01 12:57:28 +0200

    init

--- main.cpp
+++ main.cpp
@@ -1,57 +1,232 @@
-#include <iostream>
-#include <array>
+#include <bits/stdc++.h>
+#include <conio.h>
 
-#include <Helper.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
+class incasari {
+    private:
+    int A;
+    int B;
+    int C;
+    int D;
+    int total;
+    public:
+    incasari() {
+        this->A = 0;
+        this->B = 0;
+        this->C = 0;
+        this->D = 0;
     }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
+    void input(int A,int B, int C, int D, int total) {
+        this->A = A;
+        this->B = B;
+        this->C = C;
+        this->D = D;
+        this->total = total;
     }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
+    void display() {
+        cout<<setw(10)<<"Categoria A:"<<setw(10)<<"Categotia B:"<<setw(10)<<"Categoria C:"<<setw(10)<<"Categoria D:";
+        cout<<endl;
+        cout<<setw(9)<<this->A<<' '<<setw(9)<<this->B<<' '<<setw(9)<<this->C<<' '<<setw(9)<<this->D<<endl;
+    }
+
+};
+
+class tip_vehicul {
+private:
+    char nume_tip_vehicul[100];
+public:
+    tip_vehicul() {
+        strcpy(nume_tip_vehicul,"vehicul");
+    }
+    void setare_tip_vehicul(incasari &Incasari) {
+while(1){
+        cout<<"Selecteaza categoria vehiculuilui: "<<endl;
+        cout<<"1.A"<<' '<<"2.B"<<' '<<"3.C"<<' '<<"4.D"<<endl;
+        int tip_vehicul;
+        cin>>tip_vehicul;
+        if(tip_vehicul==1) {
+            strcpy(nume_tip_vehicul,"A");
+            Incasari.input(20,0,0,0,20);
+            break;
+        }
+        else if(tip_vehicul==2) {
+            strcpy(nume_tip_vehicul,"B");
+            Incasari.input(0, 20,0,0,20);
+            break;
+        }
+        else if(tip_vehicul==3) {
+            strcpy(nume_tip_vehicul,"C");
+            Incasari.input(0,0,60,0,60);
+            break;
+        }
+        else if(tip_vehicul==4) {
+            strcpy(nume_tip_vehicul,"D");
+            Incasari.input(0,0,0,100,100);
+            break;
+        }
+        else {
+            cout<<"Eroare la intrare"<<endl;
+        }
+}
+    }
+    void display_tip_vehicul() {
+        cout<<nume_tip_vehicul<<' '<<endl;
+    }
+};
+
+class vehicul:public tip_vehicul {
+private:
+    int nr_vehicul;//numarul de intrare a vehiculuilui in baza de date a parcarii
+    char nr_matriculare[10];
+    char culoare[10];
+    char marca[20];
+    char model[20];
+    char ora_intrare[10];
+    char ora_iesire[10];
+    incasari Incasari;
+public:
+    vehicul()
+{
+    this->nr_vehicul = 0;
+        strcpy(this->nr_matriculare,"default");
+        strcpy(this->culoare,"default");
+        strcpy(this->marca,"default");
+        strcpy(this->model,"default");
+        time_t tt;
+        time(&tt);
+        strcpy(this->ora_intrare,asctime(localtime(&tt)));
+        strcpy(this->ora_iesire,asctime(localtime(&tt)));
+}
+    void intrare(){
+        cout<<"Introduceti numarul vehiculului "<<endl;
+        cin>>nr_vehicul;
+        cout<<"Introduceti numarul matriculare "<<endl;
+        cin.getline(nr_matriculare,10);
+        cout<<"Introduceti culoarea "<<endl;
+        cin>>culoare;
+        cout<<"Introduceti marca "<<endl;
+        cin>>marca;
+        cout<<"Introduceti model "<<endl;
+        cin>>model;
+        setare_tip_vehicul(Incasari);
+        time_t tt;
+        time(&tt);
+        strcpy(this->ora_intrare,asctime(localtime(&tt)));
+        strcpy(this->ora_iesire,asctime(localtime(&tt)));
+    }
+    void iesire() {
+        cout<<"Numarul vehicului "<<nr_vehicul<<endl;
+        cout<<"Numarul matriculare "<<nr_matriculare<<endl;
+        cout<<ora_iesire<<" - "<<ora_iesire<<endl;
+    }
+    void afisare_vehicul() {
+        cout<<nr_vehicul<<' '<<marca<<' '<<culoare<<endl;
+        cout<<nr_vehicul;
+        Incasari.display();
+    }
+    void adauga_vehicul();
+    void adauga_in_lista();
+    void cautare_vehicul(int nr);
+    void stergere_vehicul(int nr);
+};
+//nu
+/*
+void vehicul::adauga_vehicul() {
+    ofstream g;
+    g.open("add_data.txt",ios_base::app|ios_base::binary);
+    getch();
+    g.write((char*)this,sizeof(*this));
+    g.close();
+}
+
+void vehicul::adauga_in_lista() {
+    ifstream f;
+    int n=0;
+    f.open("add_data.txt",ios_base::in|ios_base::binary);
+    if(!f) {
+        cout<<"Fisier lipsa"<<endl;
+    }
+    else {
+        f.read((char*)this,sizeof(*this));
+        while(!f.eof()) {
+        this->afisare_vehicul();
+            n++;
+            f.read((char*)this,sizeof(*this));
+        }
+        f.close();
+    }
+}
+
+void vehicul::cautare_vehicul(int nr) {
+    ifstream f;
+    int n=0;
+    f.open("add_data.txt",ios_base::in|ios_base::binary);
+    if(!f) {
+        cout<<"Fisier lipsa"<<endl;
+    }
+    else {
+        f.read((char*)this,sizeof(*this));
+        while(!f.eof()) {
+            if(this->nr_vehicul==nr) {
+                this->afisare_vehicul();
+                n=1;
+                break;
+            }
+            f.read((char*)this,sizeof(*this));
+        }
+        f.close();
+        if(nr==0)
+            cout<<"Lipsa date"<<endl;
+    }
+}
+
+void vehicul::stergere_vehicul(int nr) {
+    ifstream f;
+    ofstream g;
+    int flg=0;
+    f.open("add_data.txt",ios_base::in|ios_base::binary);
+    if(!f) {
+        cout<<"Fisier lipsa"<<endl;
+    }
+    else {
+        f.read((char*)this,sizeof(*this));
+        g.open("add_temp_data.txt",ios_base::in|ios_base::binary);
+        while(!f.eof()) {
+            if(this->nr_vehicul==nr) {
+                flg=1;
+            }
+            else {
+                g.write((char*)this,sizeof(*this));
+            }
+            f.read((char*)this,sizeof(*this));
+        }
+        f.close();
+        g.close();
+        if(nr==0){
+            cout<<"Lipsa date, nu putrm sterge obiectul "<<endl;
+            getch();
+            remove("add_data.txt");
+        }
+        else {
+            cout<<"Obiect sters"<<endl;
+            getch();
+            remove("add_data.txt");
+            getch();
+            rename("add_temp_data.txt","add_data.txt");
+        }
+    }
+}
+*/
+
+int main()
+{
+    system("cls");
+    vehicul v;
+    v.intrare();
+    v.afisare_vehicul();
+    v.adauga_in_lista();
+    v.stergere_vehicul(100);
+    getch();
     return 0;
 }
